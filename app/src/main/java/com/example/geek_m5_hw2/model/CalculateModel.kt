ad20b98e8db58fc54fa1cd2b88d979dc5fa4ce0b
Alymbek28 <89205143+Alymbek28@users.noreply.github.com>
2023-03-03 23:32:46 +0600

    Initial commit

---
+++ app/src/main/java/com/example/geek_m5_hw2/model/CalculateModel.kt
@@ -0,0 +1,16 @@
+package com.example.geek_m5_hw2.model
+
+data class CalculateModel
+
+    (
+
+     val fname:String,
+
+     val sname:String,
+
+     val percentage:String,
+
+     val result:String
+):java.io.Serializable
+
+
